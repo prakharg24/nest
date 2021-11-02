959180351a15a81f9f75aba83a820a5320083cca
PRAKHAR GANESH <pganesh@xgpf10.comp.nus.edu.sg>
2021-11-02 19:12:20 +0800

    demo loop

--- demo.py
+++ demo.py
@@ -1,15 +1,49 @@
-### Import all components. There should a simple python file for each component which contains all required functions and a link to the component folder. Check structure.md for details
+### Import all components.
+### There should a simple python file for each component which contains all required functions and a link to the component folder.
+### Check structure.md for details
+### Example,
+### from ppcm import generate_dialogue
+### from emotion_classifier import tokeniser, classification, scorer
+### etc. etc.
 
 ### Start with some conversation
+### We need a starting point of conversation. Let us use some example from dataset and hardcode it for now
+print("Conversation Starts")
 
-### Create a form of loop which takes inputs from the user and shows the agent's outputs and other meta-data
+conv_array = []
+conv_array.append({'text': 'Hello there! Are you getting excited for your upcoming trip?! I am so very excited to test my skills!',
+                   'id': 'human', 'task_data': {}})
 
-### Pass the sentence through the parser for classes
+print("Human : %s" % conv_array[0]['text'])
 
-### Use the classes and the agent to predict next set of classes
+meta_data_seq = []
+while True:
+    ### Call the parser (or multiple different parsers as required) to extract relevant meta-data
+    meta_data = dummy_parser(conv_array[-1]['text'])
+    meta_data_seq.append(meta_data)
 
-### Use the predicted classes and the conversation till now to generate dialogue
+    ### Call the agent with the new set of metadata
+    output_meta_data, output_proposal = dummy_manager(meta_data_seq)
+    meta_data_seq.append(output_meta_data)
 
-### Post-process the dialogue
+    ### Check if the negotiation has moved to proposal stage
+    if output_proposal is not None:
+        conv_array.append({'text': output_proposal['proposal_phrase'], 'id': 'agent', 'task_data': output_proposal[task_data]})
+        print("Agent : %s" % conv_array[-1]['text'])
+        print("Firewood : %d" % conv_array[-1]['task_data']['Firewood'])
+        print("Water : %d" % conv_array[-1]['task_data']['Water'])
+        print("Food : %d" % conv_array[-1]['task_data']['Food'])
+    else:
+        ### Call the generator to produce the output dialogue for the agent
+        out_dialogue = dummy_generator(conv_array, output_meta_data)
+        conv_array.append({'text': out_dialogue['text'], 'id': 'agent', 'task_data': {}})
+        print("Agent : %s" % conv_array[-1]['text'])
 
-### print out the dialogue and end the loop
+    ### Check if the negotiation has ended
+
+    ### Ask for the Human's Input
+    user_inp = input("Human : ")
+
+    ### Check if the human input was a proposal and ask for extra inputs if required
+
+    ### Check again if the negotiation has ended
