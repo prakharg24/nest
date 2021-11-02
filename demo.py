1b2f2bf719a6b668bbf2a714920983a6ccafbc3b
prakharg24 <prakhargannu@gmail.com>
2021-11-02 16:59:43 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,1 +1,15 @@
+### Import all components. There should a simple python file for each component which contains all required functions and a link to the component folder. Check structure.md for details
 
+### Start with some conversation
+
+### Create a form of loop which takes inputs from the user and shows the agent's outputs and other meta-data
+
+### Pass the sentence through the parser for classes
+
+### Use the classes and the agent to predict next set of classes
+
+### Use the predicted classes and the conversation till now to generate dialogue
+
+### Post-process the dialogue
+
+### print out the dialogue and end the loop
